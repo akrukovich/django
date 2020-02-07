0a5bd7627445d66ab0daa64656360fe006f6878b
akrukovich <anatoliinakrukovich@example.com>
2020-02-07 14:11:14 +0200

    css aws changes

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -138,9 +138,9 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'anatoliinakrukovich@gmail.com'
 EMAIL_HOST_PASSWORD = 'zazohkitkohtiwpx'
 
-# AWS_ACCESS_KEY_ID =os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID =os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
 # AWS_ACCESS_KEY_ID ='AKIAU3S3MHT6EBODGYAB'
 #
 #
@@ -148,9 +148,9 @@ EMAIL_HOST_PASSWORD = 'zazohkitkohtiwpx'
 #
 #
 # AWS_STORAGE_BUCKET_NAME='krukovich-knows-best-files'
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
